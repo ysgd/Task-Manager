a09f051ef1df60f9cc0890f384e49fe33d18f27b
Yaju Shrestha <yajustha11@gmail.com>
2024-03-31 21:21:54 +0545

    rendered items in list, added edit, delete buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,6 +47,53 @@ class App extends Component {
       </div>
     )
   }
+
+  // Rendering all items in list
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.taskList.filter(
+      item => item.completed == viewCompleted
+    );
+  
+
+  return newItems.map(item => (
+    <li key={item.id}
+    className='list-group-item d-flex justify-content-between align-items-center'>
+      <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}>
+        {item.title}
+      </span>
+      <span>
+        <button className='btn btn-info mr-2'>Edit</button>
+        <button className='btn btn-danger mr-2'>Delete</button>
+      </span>
+    </li>
+  ))
+
+};
+  
+  render(){
+    return (
+      <main className="context">
+        <h1 className="text-black text-uppercase text-center my-4">Task Manager</h1>
+        <h1 className="row">
+          <div className="col-md-6 col-sma-10 mx-auto p-0">
+            <div className="card p-3">
+              <div>
+                <button className='btn btn-dark'>Add Task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className='list-group list-group-flush'>
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </h1>
+      </main>
+    )
+  }
+
+
+ 
 }
 
 
